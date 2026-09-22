d1e9cc392d7ea062c9f2f4c70f46d88d7ff7bc60
aakash0101x <ak.developers0101@gmail.com>
2026-09-23 02:07:33 +0530

    chat feature development started

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -71,4 +71,34 @@ export const verifyUser = TryCatch(async(req,res)=>{
 export const myProfile = TryCatch(async(req: AuthenticatedRequest, res)=>{
     const user = req.user
     res.json(user);
-})
+});
+
+export const updateName = TryCatch(async(req: AuthenticatedRequest, res)=>{
+    const user = await User.findById(req.user?._id);
+    if(!user){
+        res.status(404).json({
+            message: "Please Login"
+        });
+        return;
+    }
+    
+    user.name = req.body.name;
+    await user.save();
+    
+    const token = generateToken(user);
+    res.json({
+        message: "User Updated",
+        user,
+        token
+    });
+});
+
+export const getAllUsers = TryCatch(async(req: AuthenticatedRequest, res)=>{
+    const users = await User.find();
+    res.json(users);
+});
+
+export const getAUser = TryCatch(async(req: AuthenticatedRequest, res)=>{
+    const user = await User.findById(req.params.id);
+    res.json(user);
+});
